17cf52accdf1e3f88825a5b6f4c0395bc4b05109
Sojo506 <fsojodev@gmail.com>
2024-08-28 07:55:34 -0600

    Add technologies data to map them into Technologies.jsx

---
+++ src/assets/technologies.js
@@ -0,0 +1,17 @@
+const technologies = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React.JS',
+  'Next.JS',
+  'Java',
+  'Python',
+  'PHP',
+  'Tailwind CSS',
+  'Node.JS',
+  'Express',
+  'MongoDB',
+  'PostgresSQL',
+];
+
+export default technologies;
