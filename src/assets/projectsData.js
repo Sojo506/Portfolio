01c8b57dcb275044b041bd110355f13dfb214d6d
Sojo506 <fsojodev@gmail.com>
2024-08-28 07:46:23 -0600

    Add projects data to map into Projecs.jsx

---
+++ src/assets/projectsData.js
@@ -0,0 +1,42 @@
+const projectsData = [
+  {
+    name: 'Pokemon',
+    image: '/pokemon.png',
+    description: 'A full stack project. Frontend - Backend.',
+    github: 'https://github.com/Sojo506/Pokemon-Project?tab=readme-ov-file',
+    livepreview:
+      'https://github.com/Sojo506/Pokemon-Project?tab=readme-ov-file',
+  },
+  {
+    name: 'Food_Del',
+    image: '/food-del.png',
+    description: 'A full stack project. Frontend - Backend - Admin Panel.',
+    github: 'https://github.com/Sojo506/front-food-del',
+    livepreview: 'https://front-food-del.vercel.app/',
+  },
+  {
+    name: 'Veterinary_Patient_Administrator',
+    image: '/vpa.png',
+    description: 'A full stack project. Frontend - Backend - Admin Panel.',
+    github: 'https://github.com/Sojo506/vpa_frontend',
+    livepreview: 'https://vpa-frontend.vercel.app/',
+  },
+  {
+    name: 'Edusity_Landing',
+    image: '/edusity.png',
+    description: 'A landing page about a college.',
+    github: 'https://github.com/Sojo506/edusity',
+    livepreview: 'https://edusity-project-zeta.vercel.app/',
+  },
+  {
+    name: 'Encryptor',
+    image: '/encryptor.png',
+    description: 'A encryptor page.',
+    github:
+      'https://github.com/Sojo506/oracle-one-alura-latam-encryption-challenge',
+    livepreview:
+      'https://sojo506.github.io/oracle-one-alura-latam-encryption-challenge/',
+  },
+];
+
+export default projectsData;
