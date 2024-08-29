c17b45c493099bd51b81749137072e3497deedfb
Sojo506 <fsojodev@gmail.com>
2024-08-29 00:23:38 -0600

    Add one more project, and modify img height

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,18 +1,26 @@
 const projectsData = [
   {
-    name: 'Pokemon',
-    image: '/pokemon.png',
-    description: 'A full stack project. Frontend - Backend.',
-    github: 'https://github.com/Sojo506/Pokemon-Project?tab=readme-ov-file',
-    livepreview:
-      'https://github.com/Sojo506/Pokemon-Project?tab=readme-ov-file',
-  },
-  {
     name: 'Food_Del',
     image: '/food-del.png',
     description: 'A full stack project. Frontend - Backend - Admin Panel.',
     github: 'https://github.com/Sojo506/front-food-del',
     livepreview: 'https://front-food-del.vercel.app/',
+  },
+  {
+    name: 'Lazy_buy',
+    image: '',
+    description: 'A full stack project. Frontend - Backend - Admin Panel.',
+    github: 'https://github.com/Sojo506/front-food-del',
+    livepreview: 'https://www.youtube.com/watch?v=Q5lRYMJ8aM0&t',
+  },
+
+  {
+    name: 'Pokemon',
+    image: '/pokemon.png',
+    description: 'A full stack project. Frontend - Backend.',
+    github: 'https://github.com/Sojo506/Pokemon-Project?tab=readme-ov-file',
+    livepreview:
+      'https://pokemon-project-ijsqi6lut-sojo506s-projects.vercel.app/',
   },
   {
     name: 'Veterinary_Patient_Administrator',
@@ -31,7 +39,7 @@ const projectsData = [
   {
     name: 'Encryptor',
     image: '/encryptor.png',
-    description: 'A encryptor page.',
+    description: 'An encryptor page.',
     github:
       'https://github.com/Sojo506/oracle-one-alura-latam-encryption-challenge',
     livepreview:
